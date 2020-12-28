fe21f6392908a3b754013d0d320e1ca410d97b65
Casper Bojer <casperbojer@gmail.com>
2020-12-28 15:39:47 +0100

    Format using black

--- forestknn/forestknn.py
+++ forestknn/forestknn.py
@@ -1,15 +1,19 @@
 import numpy as np
 from forestknn.utils import *
 
+
 def matching_leaf_indexes(x, leaf_idx):
     return (x == leaf_idx).astype(int)
+
 
 def get_matching_leaf_indexes(xs, leaf_idxs):
     matches = list(map(lambda x: matching_leaf_indexes(*x), zip(xs.T, leaf_idxs.T)))
     return np.vstack(matches)
 
+
 def calculate_neighbor_weights(match_idxs):
-    return match_idxs.sum(axis=0)/match_idxs.sum()
+    return match_idxs.sum(axis=0) / match_idxs.sum()
+
 
 def neighbor_weights(estimator, neighborhood, query):
     neighborhood_leaf_idxs = estimator.apply(neighborhood)
@@ -18,8 +22,9 @@ def neighbor_weights(estimator, neighborhood, query):
     wts = calculate_neighbor_weights(matches)
     return wts
 
-def adaptive_k_nearest_neighbors(estimator, neighborhood, query, k = 10):
+
+def adaptive_k_nearest_neighbors(estimator, neighborhood, query, k=10):
     neighbor_wts = neighbor_weights(estimator, neighborhood, query)
     top_k = get_top_k(neighbor_wts, k)
-    knns = neighborhood.iloc[top_k.index].assign(wts = lambda x: top_k.values)
+    knns = neighborhood.iloc[top_k.index].assign(wts=lambda x: top_k.values)
     return knns
--- notebooks/example.py
+++ notebooks/example.py
@@ -6,11 +6,13 @@ from forestknn import *
 path = "~/Documents/kaggle/walmart/"
 
 # %%
-data = pd.read_csv(path + "train.csv", parse_dates=True) \
-    .assign(Date = lambda x: pd.to_datetime(x["Date"])) \
-    .assign(month = lambda x: x["Date"].dt.month) \
-    .assign(day = lambda x: x["Date"].dt.day) \
-    .assign(dayofyear = lambda x: x["Date"].dt.dayofyear)
+data = (
+    pd.read_csv(path + "train.csv", parse_dates=True)
+    .assign(Date=lambda x: pd.to_datetime(x["Date"]))
+    .assign(month=lambda x: x["Date"].dt.month)
+    .assign(day=lambda x: x["Date"].dt.day)
+    .assign(dayofyear=lambda x: x["Date"].dt.dayofyear)
+)
 x = data.iloc[0:10000].drop(["Weekly_Sales", "Date"], axis=1)
 y = data.iloc[0:10000].loc[:, "Weekly_Sales"]
 
@@ -19,9 +21,11 @@ rf = RandomForestRegressor()
 rf.fit(x, y)
 
 #%%
-q = data.iloc[100100:100101,:].drop(["Weekly_Sales", "Date"], axis=1); q
+q = data.iloc[100100:100101, :].drop(["Weekly_Sales", "Date"], axis=1)
+q
 #%%
-knn = adaptive_k_nearest_neighbors(rf, x, q); knn
+knn = adaptive_k_nearest_neighbors(rf, x, q)
+knn
 
 #%%
 from sklearn.inspection import permutation_importance
